ca22642671ffc82e4c506bdd5127dcc3a6cdc169
Madhav singh <madhav.qg@gmail.com>
2019-07-01 14:20:18 +0530

    Initial Commit

---
+++ Lobby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lobby
+//
+//  Created by QuietGrowth pty ltd on 01/07/19.
+//  Copyright © 2019 Mrajsingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LobbyTests/LobbyTests.swift
@@ -0,0 +1,34 @@
+//
+//  LobbyTests.swift
+//  LobbyTests
+//
+//  Created by QuietGrowth pty ltd on 01/07/19.
+//  Copyright © 2019 Mrajsingh. All rights reserved.
+//
+
+import XCTest
+@testable import Lobby
+
+class LobbyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
